924335958ff86f919b8e7e5681b43827a33b73a6
Bill Sideris <bill88t@feline.gr>
2024-03-31 04:08:25 +0300

    Do not let the flash float

--- ports/espressif/boards/ai_thinker_esp32_cam/board.c
+++ ports/espressif/boards/ai_thinker_esp32_cam/board.c
@@ -25,5 +25,21 @@
  */
 
 #include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "shared-bindings/microcontroller/Pin.h"
+#include "components/driver/gpio/include/driver/gpio.h"
+#include "components/hal/include/hal/gpio_hal.h"
+#include "common-hal/microcontroller/Pin.h"
+
+void board_init(void) {
+    reset_board();
+}
+
+void reset_board(void) {
+    // Do not let the flash float.
+
+    gpio_set_direction(4, GPIO_MODE_DEF_OUTPUT);
+    gpio_set_level(4, false);
+}
 
 // Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
